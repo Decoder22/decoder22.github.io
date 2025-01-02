9159f57a7b5dc2b388a68bf137702fef108953bd
Brian Johncox <brianjohncox232@gmail.com>
2025-01-02 17:16:40 -0500

    Updates

--- static/js/949.32869d2f.chunk.js
+++ static/js/949.32869d2f.chunk.js
@@ -1,4 +1,4 @@
-"use strict";(self.webpackChunkbjohncox_website=self.webpackChunkbjohncox_website||[]).push([[949],{9397:(e,t,o)=>{o.r(t),o.d(t,{default:()=>k});var i=o(5043),r=o(1529);const n=o.p+"static/media/logo.ebcd58b72318326de48c.png";var s=o(9473),c=o(579);const a=r.Ay.div`
+"use strict";(self.webpackChunkbjohncox_website=self.webpackChunkbjohncox_website||[]).push([[949],{9397:(e,t,o)=>{o.r(t),o.d(t,{default:()=>k});var i=o(5043),r=o(1529);const s=o.p+"static/media/logo.ebcd58b72318326de48c.png";var n=o(9473),c=o(579);const a=r.Ay.div`
     height: 5rem;
     width: 100vw;
     max-width: 80rem;
@@ -20,20 +20,7 @@
     display:flex;
     justify-content: center;
     align-items: center;
-`,h=((0,r.Ay)(s.N_)`
-    padding: 1rem;
-    cursor: pointer;
-    border-radius: 0.25rem;
-
-    &:hover {
-        background-color: blue;
-    }
-
-    &.active {
-
-        background-color: blue;
-    }
-`,(0,r.Ay)(s.N_)`
+`,h=(0,r.Ay)(n.N_)`
   background: none;
   border: none;
   color: ${e=>{let{theme:t}=e;return t.colors.buttonText}};
@@ -58,7 +45,7 @@
   &:hover::after {
     width: 100%;
   }
-`);function m(){return(0,c.jsxs)(a,{children:[(0,c.jsx)(l,{src:n,alt:"Logo",onClick:()=>{window.scrollTo({top:0,behavior:"smooth"})}}),(0,c.jsxs)(d,{children:[(0,c.jsx)(h,{activeClass:"active",to:"hero",spy:!0,smooth:!0,offset:-80,duration:500,children:"Home"}),(0,c.jsx)(h,{activeClass:"active",to:"AboutMe",spy:!0,smooth:!0,offset:-80,duration:500,children:"About"}),(0,c.jsx)(h,{activeClass:"active",to:"projects",spy:!0,smooth:!0,offset:-80,duration:500,children:"Portfolio"}),(0,c.jsx)(h,{activeClass:"active",to:"contact",spy:!0,smooth:!0,offset:-80,duration:500,children:"Contact"})]})]})}var u=o(3586);const p=r.Ay.div`
+`;function m(){return(0,c.jsxs)(a,{children:[(0,c.jsx)(l,{src:s,alt:"Logo",onClick:()=>{window.scrollTo({top:0,behavior:"smooth"})}}),(0,c.jsxs)(d,{children:[(0,c.jsx)(h,{activeClass:"active",to:"hero",spy:!0,smooth:!0,offset:-80,duration:500,children:"Home"}),(0,c.jsx)(h,{activeClass:"active",to:"AboutMe",spy:!0,smooth:!0,offset:-80,duration:500,children:"About Me"}),(0,c.jsx)(h,{activeClass:"active",to:"projects",spy:!0,smooth:!0,offset:-80,duration:500,children:"Projects"}),(0,c.jsx)(h,{activeClass:"active",to:"contact",spy:!0,smooth:!0,offset:-80,duration:500,children:"Contact"})]})]})}var u=o(3586);const p=r.Ay.div`
     position: absolute;
     height: 2rem;
     top: calc(2.5rem - 1rem); // Half of 5rem nav height minus half of button height
@@ -73,7 +60,7 @@
 `,f=r.Ay.div`
     position: relative;
     height: 100%;
-`,v=r.Ay.div`
+`,x=r.Ay.div`
     display: ${e=>e.isOpen?"flex":"none"};
     position: fixed;
     top: 0;
@@ -85,7 +72,7 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-`,x=(0,r.Ay)(s.N_)`
+`,v=(0,r.Ay)(n.N_)`
     padding: 1rem;
     color: ${e=>{let{theme:t}=e;return t.colors.text}};
     font-size: 1.5rem;
@@ -95,21 +82,21 @@
     &:hover {
         color: ${e=>{let{theme:t}=e;return t.colors.primary}};
     }
-`;function b(){const[e,t]=(0,i.useState)(!1);return(0,c.jsxs)(f,{children:[(0,c.jsx)(p,{onClick:()=>t(!e),children:(0,c.jsx)(u.A,{})}),(0,c.jsxs)(v,{isOpen:e,children:[(0,c.jsx)(x,{activeClass:"active",to:"hero",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"Home"}),(0,c.jsx)(x,{activeClass:"active",to:"AboutMe",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"About"}),(0,c.jsx)(x,{activeClass:"active",to:"projects",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"Portfolio"}),(0,c.jsx)(x,{activeClass:"active",to:"contact",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"Contact"})]})]})}const y=r.Ay.nav`
+`;function y(){const[e,t]=(0,i.useState)(!1);return(0,c.jsxs)(f,{children:[(0,c.jsx)(p,{onClick:()=>t(!e),children:(0,c.jsx)(u.A,{})}),(0,c.jsxs)(x,{isOpen:e,children:[(0,c.jsx)(v,{activeClass:"active",to:"hero",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"Home"}),(0,c.jsx)(v,{activeClass:"active",to:"AboutMe",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"About Me"}),(0,c.jsx)(v,{activeClass:"active",to:"projects",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"Projects"}),(0,c.jsx)(v,{activeClass:"active",to:"contact",spy:!0,smooth:!0,offset:-80,duration:500,onClick:()=>t(!1),children:"Contact"})]})]})}const j=r.Ay.nav`
     background: ${e=>{let{theme:t}=e;return t.colors.primary}};
     height: 5rem;
     width: 100vw;
     position: sticky;
     top: 0;
-    z-index: 3;
-`,g=r.Ay.div`
+    z-index: 2000;
+`,b=r.Ay.div`
     @media screen and (max-width: 720px) {
         display: none;
     }
-`,j=r.Ay.div`
+`,g=r.Ay.div`
     display: none;
     @media screen and (max-width: 720px) {
         display: block;
     }
-`;function k(){return(0,c.jsxs)(y,{children:[(0,c.jsx)(g,{children:(0,c.jsx)(m,{})}),(0,c.jsx)(j,{children:(0,c.jsx)(b,{})})]})}}}]);
-//# sourceMappingURL=949.42b0aa7c.chunk.js.map
+`;function k(){return(0,c.jsxs)(j,{children:[(0,c.jsx)(b,{children:(0,c.jsx)(m,{})}),(0,c.jsx)(g,{children:(0,c.jsx)(y,{})})]})}}}]);
+//# sourceMappingURL=949.32869d2f.chunk.js.map
